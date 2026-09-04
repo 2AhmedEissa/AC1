5519422590e84ca3d6c1b537a8c30b38d30aa378
2AhmedEissa <ahmadoeissa@gmail.com>
2026-09-04 16:24:32 +0300

    Done

---
+++ AC1/Generic Constraints/Constraints.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AC1.Generic_Constraints
+{
+
+    #region Generic Constraints
+
+    #region Q7
+
+    internal class StructConstraint<T> where T : struct
+    {
+        public StructConstraint(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+    }
+
+
+
+
+    #endregion
+
+    #region Q8
+
+
+    internal class ClassConstraint<T> where T : class
+    {
+        public ClassConstraint(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+    }
+
+
+
+    #endregion
+
+    #region Q9
+
+    internal class newConstraint<T> where T : new()
+    {
+        public T Create()
+        {
+            return new T();
+        }
+
+    }
+
+
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Name = "Hamada";
+            Age = 24;
+        }
+
+        public override string ToString() => $"Name: {Name}, Age: {Age}";
+
+    }
+
+
+    #endregion
+
+    #region Q10
+
+    internal class Comaparer
+    {
+        public static T FindMax<T>(T[] items) where T : IComparable<T>
+        {
+            T max = items[0];
+            foreach (T item in items)
+            {
+                if (item.CompareTo(max) > 0)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+        public static T FindMin<T>(T[] items) where T : IComparable<T>
+        {
+            T min = items[0];
+            foreach (T item in items)
+            {
+                if (item.CompareTo(min) < 0)
+                {
+                    min = item;
+                }
+            }
+            return min;
+        }
+    }
+
+    class Student : IComparable<Student>
+    {
+        public int Grade { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            return Grade.CompareTo(other.Grade);
+        }
+    }
+
+    class Student2
+    {
+        public int Grade { get; set; }
+
+        public int CompareTo(Student2 other)
+        {
+            return Grade.CompareTo(other.Grade);
+        }
+    }
+
+
+    #endregion
+
+    #region Q11
+
+
+
+
+
+
+    #endregion
+
+    #region Q12
+
+
+
+
+
+
+    #endregion
+
+
+
+    #endregion
+
+
+
+
+
+
+}
--- AC1/Program.cs
+++ AC1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AC1.Generic_Class;
+using AC1.Generic_Constraints;
 using AC1.Generic_Interface;
 
 namespace AC1
@@ -6,7 +7,238 @@ namespace AC1
     internal class Program
     {
 
+        #region Q11 - Base Class Constraint
 
+        // A base class constraint requires T to inherit from a specific class.
+
+        class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Eating...");
+            }
+        }
+
+        class Dog : Animal
+        {
+        }
+
+        class AnimalHelper<T> where T : Animal
+        {
+            public void MakeEat(T animal)
+            {
+                animal.Eat();
+            }
+        }
+
+        #endregion
+
+        #region Q12 - Multiple Constraints
+
+        // By using commas.
+
+        class Student : IComparable<Student>
+        {
+            public int Grade { get; set; }
+
+            public int CompareTo(Student other)
+            {
+                return Grade.CompareTo(other.Grade);
+            }
+        }
+
+        class StudentHelper<T>
+            where T : class, IComparable<T>, new()
+        {
+            public T Create()
+            {
+                return new T();
+            }
+        }
+        #endregion
+
+        #region Q13 - default Keyword in Generics
+
+        // default(T) returns the default value of the type T.
+
+        public static T GetDefault<T>()
+        {
+            return default(T);
+        }
+
+        #endregion
+
+        #region Q14 - SafeList<T>
+
+        // SafeList returns default(T) when the index is invalid.
+
+        class SafeList<T>
+        {
+            private T[] _items;
+
+            public SafeList(int size)
+            {
+                _items = new T[size];
+            }
+
+            public void Add(int index, T item)
+            {
+                if (index >= 0 && index < _items.Length)
+                {
+                    _items[index] = item;
+                }
+            }
+
+            public T Get(int index)
+            {
+                if (index >= 0 && index < _items.Length)
+                {
+                    return _items[index];
+                }
+
+                return default(T);
+            }
+        }
+
+        #endregion
+
+        #region Q15 - Covariance and out
+
+        // Covariance allows you to use a more derived type where a less derived type is expected.
+
+        // The 'out' keyword means the generic type is mainly used as an OUTPUT/return type.
+
+
+        #endregion
+
+        #region Q16 - Contravariance and in
+
+        // Contravariance allows you to use a less derived type where a more derived type is expected.
+
+        // The 'in' keyword means the generic type is used as an INPUT/parameter.
+
+
+        #endregion
+
+        #region Q17 - Diff
+
+        /*
+         
+         Aspect	Covariance (out)	Contravariance (in)
+         Direction	Derived → Base	Base → Derived
+         T Position	Output only (return)	Input only (parameter)
+         Example	IEnumerable<out T>	Action<in T>
+         Think of as	Producer of T	Consumer of T
+         
+         
+         
+         
+         */
+
+        #endregion
+
+        #region Q18 - Static Members in Generic Types
+
+        // Each closed generic type has its own copy of static fields. Counter<int> and Counter<string> have separate static data.
+
+        class Counter<T>
+        {
+            public static int Count = 0;
+
+            public Counter()
+            {
+                Count++;
+            }
+        }
+
+        #endregion
+
+        #region Q19 - Inheriting from a Generic Class
+
+        // Generic classes can inherit from other generic or non-generic classes.
+
+        class Repository<T>
+        {
+            public void Add(T item)
+            {
+                Console.WriteLine($"Added: {item}");
+            }
+        }
+
+        class StudentRepository : Repository<Student>
+        {
+        }
+
+        #endregion
+
+        #region Q20 - Generic Cache<TKey, TValue>
+
+        class Cache<TKey, TValue>
+        {
+            private class CacheItem
+            {
+                public TValue Value { get; set; }
+                public DateTime Expiration { get; set; }
+            }
+
+            private Dictionary<TKey, CacheItem> _items = new Dictionary<TKey, CacheItem>();
+
+
+            public void Add(TKey key, TValue value, TimeSpan expiration)
+            {
+                _items[key] = new CacheItem
+                {
+                    Value = value,
+                    Expiration = DateTime.Now.Add(expiration)
+                };
+            }
+
+            public TValue Get(TKey key)
+            {
+                if (!_items.ContainsKey(key))
+                {
+                    return default(TValue);
+                }
+
+                CacheItem item = _items[key];
+
+                if (DateTime.Now >= item.Expiration)
+                {
+                    _items.Remove(key);
+                    return default(TValue);
+                }
+
+                return item.Value;
+            }
+
+
+            public void Remove(TKey key)
+            {
+                _items.Remove(key);
+            }
+
+
+            public bool Contains(TKey key)
+            {
+                if (!_items.ContainsKey(key))
+                {
+                    return false;
+                }
+
+                CacheItem item = _items[key];
+
+                if (DateTime.Now >= item.Expiration)
+                {
+                    _items.Remove(key);
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+
+        #endregion
         static void Main(string[] args)
         {
             #region Q2
@@ -63,7 +295,103 @@ namespace AC1
             }
 
             Console.WriteLine($"Get By Id:  {Repo1.GetById(2)}");
-            Repo1.Delete(2);
+
+            #endregion
+
+            #region Generic Constraints
+
+
+            #region Q7
+
+            StructConstraint<int> structcon1 = new StructConstraint<int>(5);
+
+            //StructConstraint<string> structcon2 = new StructConstraint<string>("5");
+
+            #endregion
+
+            #region Q8
+
+            ClassConstraint<string> classCon2 = new ClassConstraint<string>("5");
+
+            //ClassConstraint<int> classCon1 = new ClassConstraint<int>(5);
+
+
+
+            #endregion
+
+            #region Q9
+
+            newConstraint<Person> Persons = new newConstraint<Person>();
+
+            Person P1 = Persons.Create();
+
+            P1.Name = "Ahmed";
+            P1.Age = 22;
+
+            Console.WriteLine($"Created: {P1}");
+
+
+
+            #endregion
+
+            #region Q10
+
+
+            Student[] students = {
+
+            new Student { Grade = 80 },
+            new Student { Grade = 95 },
+            new Student { Grade = 70 }
+
+            };
+
+
+            Student best = Comaparer.FindMax(students);
+
+            Console.WriteLine(best.Grade);
+
+            //Student2[] students2 = {
+            //new Student2 { Grade = 80 },
+            //new Student2 { Grade = 95 },
+            //new Student2 { Grade = 70 }
+
+            //};
+
+
+            //Student best2 = Comaparer.FindMax(students2);
+
+            //Console.WriteLine(best2.Grade);
+            #endregion
+
+
+            #endregion
+
+
+            #region Q20
+
+
+
+
+
+        Example:
+
+            Cache<string, string> cache = new Cache<string, string>();
+
+            cache.Add(
+                "username",
+                "Ahmed",
+                TimeSpan.FromSeconds(10)
+            );
+
+            Console.WriteLine(cache.Get("username"));
+
+            Console.WriteLine(cache.Contains("username"));
+
+            cache.Remove("username");
+
+            Console.WriteLine(cache.Contains("username"));
+
+            Console.WriteLine(cache.Get("username"));
 
             #endregion
 
